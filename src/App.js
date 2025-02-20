d3cd581b5b3e93a22d4c376f111c17dd3ebb092d
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-02-20 13:09:52 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useEffect, useContext } from 'react';
+import { useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
-import BooksContext from './context/books';
+import useBooksContext from './hooks/useBooksContext';
 
 function App() {
-  const { fetchBooks } = useContext(BooksContext);
+  const { fetchBooks } = useBooksContext();
 
   useEffect(() => {
     fetchBooks();
-  }, []);
+  }, [fetchBooks]);
 
   return (
     <div className='app'>
